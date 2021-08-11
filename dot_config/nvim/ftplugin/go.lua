358b5e8cdf0ccf8681ecff95d3e95be2b8b676a0
Daniel Trautmann <me@danieltrautmann.com>
2021-08-11 16:44:49 +0200

    Neovim: Run format on save for Go

--- dot_config/nvim/ftplugin/go.lua
+++ dot_config/nvim/ftplugin/go.lua
@@ -1,3 +1,9 @@
 local helpers = require('helpers')
 
+-- Set indentation
 helpers.set_indent('tabs', 4)
+
+-- Format code on save
+helpers.create_augroup({
+  {'BufWritePre', '*.go', 'lua', 'vim.lsp.buf.formatting_sync()'}
+}, 'gofmtonsave')
