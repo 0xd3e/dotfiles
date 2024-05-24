20183cc43d5d03ee08e7c5ffe16a4002a40fed9d
Daniel Trautmann <me@danieltrautmann.com>
2024-05-24 11:29:55 +0200

    flake: Use unstable packages by default

--- flake.nix
+++ flake.nix
@@ -1,52 +1,58 @@
 {
+  description = "Handles package management for my system";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable }: let
+  outputs = { self, nixpkgs-unstable, nixpkgs-stable }: let
     system = "x86_64-darwin";
-    stablePkgs = import nixpkgs { inherit system; };
-    unstablePkgs = import nixpkgs-unstable {
-      inherit system;
-    };
-    neovim = unstablePkgs.neovim-unwrapped.override rec {
+
+    unstable = import nixpkgs-unstable { inherit system; };
+    stable = import nixpkgs-stable { inherit system; };
+
+    neovim = unstable.neovim-unwrapped.override rec {
         treesitter-parsers = {};
     };
   in {
-    packages.${system}.default = stablePkgs.buildEnv {
+    packages.${system}.default = unstable.buildEnv {
       name = "My Packages";
       paths = [
-        stablePkgs.bat
-        stablePkgs.delta
-        unstablePkgs.eza
-        stablePkgs.fd
-        unstablePkgs.fzf
-        stablePkgs.gitui
-        stablePkgs.gnupg
-        stablePkgs.jq
         neovim
-        unstablePkgs.nodejs_20
-        unstablePkgs.pgcli
-        unstablePkgs.podman # Requires vfkit in PATH
-        unstablePkgs.podman-compose
-        stablePkgs.pwgen
-        unstablePkgs.python311Packages.python-lsp-server
-        stablePkgs.ripgrep
-        unstablePkgs.ruff
-        stablePkgs.yq-go
-        unstablePkgs.zsh-autopair
-        unstablePkgs.zsh-autosuggestions
-        unstablePkgs.zsh-history-substring-search
-        unstablePkgs.zsh-powerlevel10k
-        unstablePkgs.zsh-syntax-highlighting
+        unstable.bat
+        unstable.delta
+        unstable.elixir
+        unstable.elixir-ls
+        unstable.eza
+        unstable.fd
+        unstable.fzf
+        unstable.gitui
+        unstable.gnupg
+        unstable.gopls
+        unstable.jq
+        unstable.nodejs_20
+        stable.pgcli
+        unstable.podman # Requires vfkit in PATH
+        unstable.podman-compose
+        unstable.pwgen
+        unstable.python312Packages.python-lsp-server
+        unstable.ripgrep
+        unstable.ruff
+        unstable.s5cmd
+        unstable.yq-go
+        unstable.zsh-autopair
+        unstable.zsh-autosuggestions
+        unstable.zsh-history-substring-search
+        unstable.zsh-powerlevel10k
+        unstable.zsh-syntax-highlighting
       ];
     };
 
-    devShells.${system}.default = stablePkgs.mkShell {
+    devShells.${system}.default = unstable.mkShell {
       name = "dotfiles";
       packages = [
-        unstablePkgs.ansible
+        unstable.ansible
       ];
     };
   };
