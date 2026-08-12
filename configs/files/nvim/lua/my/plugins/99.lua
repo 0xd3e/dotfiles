26475e88268593feae8c28dd58e96b7e15050a4f
Daniel Trautmann <me@danieltrautmann.com>
2026-08-12 14:44:55 +0200

    Adapt nvim 99 config

--- configs/files/nvim/lua/my/plugins/99.lua
+++ configs/files/nvim/lua/my/plugins/99.lua
@@ -4,8 +4,11 @@ return {
     local _99 = require('99')
 
     _99.setup({
+      provider = _99.Providers.ClaudeCodeProvider,
+      model = 'claude-sonnet-4-6',
       md_files = {
         'AGENT.md',
+        'CLAUDE.md',
       },
     })
 
@@ -19,6 +22,10 @@ return {
 
     vim.keymap.set('v', '<leader>9s', function()
       _99.stop_all_requests()
+    end)
+
+    vim.keymap.set('n', '<leader>9m', function()
+      _99.extensions.telescope.select_model()
     end)
   end,
 }
