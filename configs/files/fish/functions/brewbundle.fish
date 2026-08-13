9e5bcb43f86af5e43b0cb3c642adafd80626f0cd
Daniel Trautmann <me@danieltrautmann.com>
2026-08-13 09:05:04 +0200

    Remove cleanup flag from brewbundle

--- configs/files/fish/functions/brewbundle.fish
+++ configs/files/fish/functions/brewbundle.fish
@@ -1,5 +1,5 @@
 function brewbundle
 	set uuid (system_profiler SPHardwareDataType | grep 'Hardware UUID' | awk '{print $3}')
 
-	brew bundle --cleanup --file ~/Development/Projects/github.com/0xd3e/dotfiles/main/Brewfile.$uuid
+	brew bundle --file ~/Development/Projects/github.com/0xd3e/dotfiles/main/Brewfile.$uuid
 end
