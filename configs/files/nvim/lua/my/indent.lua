d0b3b0159cb66a7b24fcb07e831ec7d871e2ff33
Daniel Trautmann <me@danieltrautmann.com>
2026-08-14 14:32:27 +0200

    Refine indent helper function for nvim

--- configs/files/nvim/lua/my/indent.lua
+++ configs/files/nvim/lua/my/indent.lua
@@ -1,8 +1,10 @@
+--- Settings based on https://gist.github.com/LunarLambda/4c444238fb364509b72cfb891979f1dd
 local M = {}
 
 --- Indentation settings using spaces
 function M.space(width)
   vim.opt_local.expandtab = true
+  vim.opt_local.tabstop = width
   vim.opt_local.shiftwidth = width
   vim.opt_local.softtabstop = -1
   vim.opt_local.smarttab = true
@@ -10,9 +12,11 @@ end
 
 --- Indentation settings using tabs
 function M.tab(width)
+  vim.opt_local.expandtab = false
   vim.opt_local.tabstop = width
   vim.opt_local.shiftwidth = 0
-  vim.opt_local.softtabstop = -1
+  vim.opt_local.softtabstop = 0
+  vim.opt_local.smarttab = true
 end
 
 return M
